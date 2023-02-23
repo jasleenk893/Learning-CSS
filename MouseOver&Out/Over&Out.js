cde333b7b218604e296a219eaa243f69501cd2a7
jasleenk893 <jasleenk893@gmail.com>
2023-02-23 12:25:12 +0530

    first commit

---
+++ MouseOver&Out/Over&Out.js
@@ -0,0 +1,8 @@
+function setNewImage() {
+  document.getElementById("img").src =
+    "file://C:/Users/hp/Desktop/Site/MouseOver&Out/Always.jpg";
+}
+function setOldImage() {
+  document.getElementById("img").src =
+    "file://C:/Users/hp/Desktop/Site/MouseOver&Out/Smile.jpg";
+}
